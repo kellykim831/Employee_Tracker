418935ac3b778ea4e0ce163a277bedb188a18871
Kelly Kim <kellykim408@gmail.com>
2020-10-17 22:28:44 -0700

    update seed.sql

--- schema.sql
+++ schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employeeTracker_DB;
 USE employeeTracker_DB;
 
 CREATE TABLE department(
-  id INT PRIMARY KEY,
-  name VARCHAR(30), --to hold department name
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL, 
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE employeeRole(
-  id INT PRIMARY KEY,
-  title VARCHAR(30), --to hold role title
-  salary DECIMAL, --to hold role salary
-  department_id INT, --to hold reference to department role belongs to
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL, 
+  salary DECIMAL(10.3) NULL, 
+  department_id INT NULL, 
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee(
- id INT PRIMARY KEY,
- first_name VARCHAR(30), --to hold employee first name
- last_name VARCHAR(30), --to hold employee last name
- role_id INT, --to hold reference to role employee has
- manager_id INT, --to hold reference to another employee that manager of the current employee.
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(30) NULL, 
+ last_name VARCHAR(30) NULL,
+ role_id INT NULL, 
+ manager_id INT NULL, 
+ PRIMARY KEY (id)
 );
--- seed.sql
+++ seed.sql
@@ -1,3 +1,13 @@
 /* Seeds for SQL table. We haven't discussed this type of file yet */
 USE employeeTracker_DB;
 
+/* Insert 3 Rows into your new table */
+INSERT INTO department (name)
+VALUES ("name");
+
+INSERT INTO role (name)
+VALUES ("id"), ("title"), ("salary"), ("department_id");
+
+INSERT INTO employee (name)
+VALUES ("first_name"), ("last_name"), ("role_id"), ("manager_id");
+
