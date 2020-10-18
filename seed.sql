cbdc1dabe63c62c45d9dd512f375cdcef00adc30
Kelly Kim <kellykim408@gmail.com>
2020-10-17 23:31:29 -0700

    insert into employee code in seed.sql

--- seed.sql
+++ seed.sql
@@ -24,3 +24,20 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Legal Team Lead", 250000, 4);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chan", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ashley", "Rodriguez", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tupik", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Malia", "Brown", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sarah", "Lourd", 6, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Allen", 7, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tammer", "Galal", 4, 4);
