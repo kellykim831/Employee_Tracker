d8aa4ced51d1c5d91591d37f35fa1879c8a34559
Kelly Kim <kellykim408@gmail.com>
2020-10-19 22:49:29 -0700

    create view all employee function

--- employeeTracker.js
+++ employeeTracker.js
@@ -22,7 +22,7 @@ connection.connect(function (err) {
   if (err) throw err;
   //connection.end();
   // run the start function after the connection is made to prompt the user
-  start();
+  startPrompt();
 });
 
 // function which prompts the user for what action they should take
@@ -42,29 +42,46 @@ function startPrompt() {
         "Update Employee Manager"
       ]
     })
-    .then(function(answer) {
+    .then((answer) => {
       switch (answer.action) {
-      case "View All Employees":
-        viewAllEmployees();
-        break;
+        case "View All Employees":
+          viewAllEmp();
+          break;
 
-      case "View All Employees By Department":
-        viewAllEmployeesByDepartment();
-        break;
+        case "View All Employees By Department":
+          viewAllEmpByDept();
+          break;
 
-      case "Add Employee":
-        addEmployee();
-        break;
+        case "Add Employee":
+          addEmp();
+          break;
 
-      case "Remove Employee Role":
-        removeEmployeeRole();
-        break;
+        case "Remove Employee Role":
+          removeEmpRole();
+          break;
 
-      case "Update Employee Manager":
-        updateEmployeeManager();
-        break;
+        case "Update Employee Manager":
+          updateEmpMgr();
+          break;
       }
     });
+}
+
+//View All Employees
+function viewAllEmp() {
+  //Query to view All Employees
+  let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+
+  // Query from connection
+  connection.query(query, function (err, res) {
+    if (err) return err;
+    console.log("\n");
+    console.table(res);
+
+
+    startPrompt();
+  });
+
 }
 
 
