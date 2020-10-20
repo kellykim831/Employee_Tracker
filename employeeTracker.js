153ee6bee2f60cab26342bf338f03f13559ae84b
Kelly Kim <kellykim408@gmail.com>
2020-10-20 01:31:58 -0700

    add employee function

--- employeeTracker.js
+++ employeeTracker.js
@@ -157,4 +157,68 @@ function deptPrompt(deptChoices) {
     });
 }
 
+//Create employee array
+function addEmp() {
+  console.log("Adding new employee")
+
+  let query = `SELECT r.id, r.title, r.salary FROM role r`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const roleChoices = res.map(({ id, title, salary }) => ({
+      value: id, title: `${title}`, salary: `${salary}`
+    }));
+
+    console.table(res);
+    console.log("Insert Role");
+
+    addPrompt(roleChoices);
+  });
+}
+function addPrompt(roleChoices) {
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the employee's first name?"
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the employee's last name?"
+      },
+      {
+        type: "list",
+        name: "roleId",
+        message: "What is the employee's role?",
+        choices: roleChoices
+      },
+    ])
+    .then(function (answer) {
+      console.log(answer);
+
+      let query = `INSERT INTO employee SET ?`
+      // Insert new item into table with info
+      connection.query(query,
+        {
+          first_name: answer.first_name,
+          last_name: answer.last_name,
+          role_id: answer.roleId,
+          manager_id: answer.managerId,
+        },
+        function (err, res) {
+          if (err) throw err;
+
+          console.table("response ", res);
+          console.log("New Employee Inserted Successfully!\n");
+
+          startPrompt();
+        });
+    });
+}
+
+
 
